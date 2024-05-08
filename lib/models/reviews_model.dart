96f52c898abf883546d064bd22aaabe91ad67bcb
Rehab Saad <saadrehab075@gmail.com>
2024-05-09 02:37:44 +0300

    create reviews model

---
+++ lib/models/reviews_model.dart
@@ -0,0 +1,105 @@
+class ReviewsModel {
+  int? id;
+  int? page;
+  List<Results>? results;
+  int? totalPages;
+  int? totalResults;
+
+  ReviewsModel(
+      {this.id, this.page, this.results, this.totalPages, this.totalResults});
+
+  ReviewsModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    page = json['page'];
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add( Results.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['page'] = page;
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = totalPages;
+    data['total_results'] = totalResults;
+    return data;
+  }
+}
+
+class Results {
+  String? author;
+  AuthorDetails? authorDetails;
+  String? content;
+  String? createdAt;
+  String? id;
+  String? updatedAt;
+  String? url;
+
+  Results(
+      {this.author,
+        this.authorDetails,
+        this.content,
+        this.createdAt,
+        this.id,
+        this.updatedAt,
+        this.url});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    author = json['author'];
+    authorDetails = json['author_details'] != null
+        ? AuthorDetails.fromJson(json['author_details'])
+        : null;
+    content = json['content'];
+    createdAt = json['created_at'];
+    id = json['id'];
+    updatedAt = json['updated_at'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['author'] = author;
+    if (authorDetails != null) {
+      data['author_details'] = authorDetails!.toJson();
+    }
+    data['content'] = content;
+    data['created_at'] = createdAt;
+    data['id'] = id;
+    data['updated_at'] = updatedAt;
+    data['url'] = url;
+    return data;
+  }
+}
+
+class AuthorDetails {
+  String? name;
+  String? username;
+  String? avatarPath;
+  int? rating;
+
+  AuthorDetails({this.name, this.username, this.avatarPath, this.rating});
+
+  AuthorDetails.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    username = json['username'];
+    avatarPath = json['avatar_path'];
+    rating = json['rating'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] =name;
+    data['username'] =username;
+    data['avatar_path'] =avatarPath;
+    data['rating'] =rating;
+    return data;
+  }
+}
